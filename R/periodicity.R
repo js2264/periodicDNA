5983582439d9e2173d561273e52cc4a2f71b7866
Jacques Serizay <js2264@cb-head3.gurdon.private.cam.ac.uk>
2019-09-16 17:03:42 +0100

    update

--- R/periodicity.R
+++ R/periodicity.R
@@ -1,19 +1,32 @@
-getPeriodicity <- function(
-    granges, 
+#### ---- getPeriodicity methods ---- ####
+getPeriodicity <- function(x, ...) {
+    UseMethod("getPeriodicity")
+}
+
+getPeriodicity.default <- function(seqs, ...) {
+    getPeriodicity.DNAStringSet(seqs, ...)
+}
+
+getPeriodicity.DNAStringSet <- function(
+    seqs, 
     motif = 'WW', 
-    N_MISMATCH = 0, 
     RANGE_FOR_SPECTRUM = 1:100,
     freq = 0.10, 
     period = seq(2, 20, 1),
     plot = FALSE,
     cores = 2, 
-    verbose = TRUE) {
+    verbose = TRUE
+    ) {
     # Get pairwise distances ---------------------------------------------------
     if (verbose) message("- Getting pairwise distances.")
-    seqs <- granges$seq
     dists <- parallel::mclapply(1:length(seqs), function(k) {
         seq <- seqs[k]
-        Biostrings::vmatchPattern(motif, seq, max.mismatch = N_MISMATCH, fixed = FALSE)[[1]] %>% 
+        Biostrings::vmatchPattern(
+            motif, 
+            seq, 
+            max.mismatch = 0
+            , fixed = FALSE
+        )[[1]] %>% 
             GenomicRanges::start() %>% 
             dist() %>% 
             c()
@@ -22,11 +35,15 @@ getPeriodicity <- function(
     # Fourier ------------------------------------------------------------------
     if (verbose) message("- Applying Fast Fourier Transform to the vector of distances.")
     if (max(dists) < tail(RANGE_FOR_SPECTRUM, 1)) {
-        if (verbose) message('Range (', head(RANGE_FOR_SPECTRUM, 1), ':', tail(RANGE_FOR_SPECTRUM, 1), ') is wider than any range in the current distances vector. Shortening the range from ', head(RANGE_FOR_SPECTRUM, 1), ' to ', max(dists), '...')
+        if (verbose) message(
+            'Range (', 
+            head(RANGE_FOR_SPECTRUM, 1), ':', tail(RANGE_FOR_SPECTRUM, 1), 
+            ') is wider than any range in the current distances vector. Shortening the range from ', 
+            head(RANGE_FOR_SPECTRUM, 1), ' to ', max(dists), '...'
+        )
         RANGE_FOR_SPECTRUM <- head(RANGE_FOR_SPECTRUM, 1):max(dists)
     }
-    hist <- hist(dists, breaks = seq(1, max(dists)+1, 1), plot = FALSE) %$%
-        counts
+    hist <- hist(dists, breaks = seq(1, max(dists)+1, 1), plot = FALSE)$counts
     norm_hist <- normalizeHistogram(hist)
     spectra <- norm_hist %>%
             '['(RANGE_FOR_SPECTRUM) %>% 
@@ -37,7 +54,9 @@ getPeriodicity <- function(
     }
     # Get signal-to-noise ratio ------------------------------------------------
     if (verbose) message("- Computing signal-to-noise ratios.")
-    mtm <- spectra$spec[unlist(lapply(c(1/(period)), function (freq) {idx <- which.min(abs(freq - spectra$freq))}))]
+    mtm <- spectra$spec[unlist(lapply(c(1/(period)), function (freq) {
+        idx <- which.min(abs(freq - spectra$freq))
+    }))]
     ratios <- data.frame(
         period = period,
         SNR = sapply(1:length(mtm), function(x) {mtm[x]/mean(mtm[-x])})
@@ -45,7 +64,10 @@ getPeriodicity <- function(
     # Return all results -------------------------------------------------------
     return(list(
         dists = dists, 
-        hist = hist, 
+        hist = data.frame(
+            distance = seq(1, max(dists), 1), 
+            counts = hist
+        ), 
         normalized_hist = data.frame(
             distance = seq(1, max(dists), 1), 
             norm_counts = norm_hist
@@ -60,6 +82,33 @@ getPeriodicity <- function(
         freq = freq
     ))
 }
+
+getPeriodicity.DNAString <- function(seq, motif = 'TT', subseq_len = NULL, n_occurences = 200, ...) {
+    if(is.null(subseq_len)) subseq_len <- nchar(seq) * 0.25
+    n_motif <- length(Biostrings::matchPattern(motif, seq, fixed = FALSE))
+    repets <- round(n_occurences/n_motif * 10)
+    starts <- sample(1:(length(seq)-subseq_len), repets, replace = repets > (nchar(seq) - subseq_len))
+    seqs <- lapply(seq_along(starts), function(K) {seq[starts[K]:(starts[K]+subseq_len)]}) %>% as('DNAStringSet')
+    spectra <- getPeriodicity(seqs, ...)$spectra
+    m <- max(
+        spectra$spec[
+        (which.min(abs(spectra$freq - freq))-1):(which.min(abs(spectra$freq - freq))+1)
+        ]
+    )
+    return(m)
+}
+
+getPeriodicity.GRanges <- function(
+    granges, 
+    genome = Biostrings::getSeq(BSgenome.Celegans.UCSC.ce11::BSgenome.Celegans.UCSC.ce11), 
+    ...
+)
+{
+    if (is.null(granges$seq)) granges <- withSeq(granges, genome)
+    seqs <- granges$seq
+    getPeriodicity(seqs, ...)
+}
+
 normalizeHistogram <- function(hist) {
     h <- hist
     h <- h / sum(h)
--- R/plot.R
+++ R/plot.R
@@ -7,26 +7,13 @@ plotPeriodicityResults <- function(results, HIST_YLIM = c(-1.5, 1.5)) {
         labs(x = paste0('Distance between pairs of ', results$motif), y = 'Normalized frequency', title = paste0('Normalized frequency of\ndistances between pairs of ', results$motif))
     p2 <- ggplot(results$PSD, aes(x = freq, y = PSD)) + 
         geom_point() +
+        geom_line() +
         theme_classic() + 
         labs(x = paste0(results$motif, ' frequency'), y = 'Power Spectrum Density', title = paste0('Power Spectrum Density of\n', results$motif, ' frequencies'))
     p3 <- ggplot(results$signal_to_noise_ratio, aes(x = period, y = SNR)) + 
         geom_point() +
+        geom_line() +
         theme_classic() + 
         labs(x = paste0(results$motif, ' period'), y = 'Signal to noise ratio', title = paste0('Strenght of\n', results$motif, ' periodicity'))
     return(list(p1, p2, p3))
-}
-plotSNRs <- function(list_SNRs) {
-    require(ggplot2)
-    lapply(list_SNRs, function(SNRs) {
-        ggplot(SNRs, aes(x = period, y = SNR, color = name)) + 
-            geom_point() +
-            theme_classic() + 
-            labs(
-                x = paste0(SNRs$motif, ' period'), 
-                y = 'Signal to noise ratio', 
-                title = paste0('Strenght of\n', SNRs$motif, ' periodicity'), 
-                color = 'Tissue-specific promoters'
-            ) + 
-            facet_wrap(~name)
-    })
 }
