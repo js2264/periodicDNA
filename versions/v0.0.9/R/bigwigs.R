87e9fc790fb571acef7544021a109aee4dbe0033
Jacques Serizay <js2264@cb-head3.gurdon.private.cam.ac.uk>
2019-09-16 20:58:31 +0100

    removed versions 2

--- versions/v0.0.9/R/bigwigs.R
+++
@@ -1,7 +0,0 @@
-#### ---- BigWig utils ---- ####
-scaleBigWigs <- function(bw.as.rle) {
-    l <- IRanges::RleList(lapply(bw.as.rle, function(L) {
-        S4Vectors::Rle(scale(L))
-    }))
-    return(l)
-}
