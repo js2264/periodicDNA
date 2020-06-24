6acfe7551c6e2481bf8e368de76edaea1f7d26b9
js2264 <js2264@cam.ac.uk>
2020-06-24 21:14:53 +0100

    unskip getPeriodicity works with shuffling

--- tests/testthat/test-getPeriodicity.R
+++ tests/testthat/test-getPeriodicity.R
@@ -36,7 +36,6 @@ test_that("getPeriodicity and plotPeriodicityResults works", {
 })
 
 test_that("getPeriodicity works with shuffling", {
-    skip('skip')
     expect_equal({
         data(ce11_proms)
         periodicity_result <- getPeriodicity(
